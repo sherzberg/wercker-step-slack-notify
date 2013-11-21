b2541c4190865bbed472552403a55406061e3a55
Jacco Flenter <github@putpixel.nl>
2013-11-21 14:40:15 +0100

    fix test_channel_starts_with_hash
    Test used the wrong string

--- tests/test_channel_starts_with_hash.sh
+++ tests/test_channel_starts_with_hash.sh
@@ -6,7 +6,7 @@ export WERCKER_SLACK_NOTIFY_SUBDOMAIN="wercker"
 export WERCKER_SLACK_NOTIFY_TOKEN="$1"
 export WERCKER_SLACK_NOTIFY_CHANNEL="#product"
 export WERCKER_STEP_TEMP=/tmp
-export FATAL_MESSAGE="Please specify token property"
+export FATAL_MESSAGE="Please specify the channel without the '#'"
 export FATAL_CALLED="false"
 source run.sh
 
